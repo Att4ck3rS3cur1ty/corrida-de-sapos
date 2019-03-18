8277e117d914eeac8c307690d96a2a7a0ce9b57c
att4ck3rs3cur1ty <abimaelima54@gmail.com>
2019-03-18 15:46:06 -0300

    Implementação

--- novo/jogodosapo.cpp
+++ novo/jogodosapo.cpp
@@ -7,11 +7,31 @@
 using namespace std;
 
 void AddJogador(Sapo* novo){
+	corredores[total_corredores] = novo;
+	total_corredores++;
 }
 
 void Run(){
+	dist_total = 15;
+	while(novo[i].getDistPercorrida < dist_total){
+		for(int i = 0; i < total_corredores; i++){
+			for(int j = 0; j < novo[i].getDistPercorrida; j++){
+				cout << ".";
+			}
+			cout << "Sapo " << novo[i];
+			for(int k = 0; k < dist_total; k++){
+				cout << ".";
+			}
+			novo[i].pular();
+			cout << "\n";
+			if(novo[i] >= dist_total){
+				Result(i);
+			}
+		}
+	}
 }
 
-void Result(){
+void Result(int i){
+	cout << "Vitória do Sapo " << i << "." << endl;
 }
 
--- novo/jogodosapo.h
+++ novo/jogodosapo.h
@@ -5,11 +5,13 @@
 
 class JogoDoSapo{
 	private:
-		//Sapo* corredores[10];
+		Sapo* corredores[10];
+		int total_corredores;
 	public:
 		void AddJogador(Sapo* novo);
-		void Run();
+		void Run(int i);
 		void Resultado();
+		static int dist_total;
 		JogoDoSapo();
 };
 
